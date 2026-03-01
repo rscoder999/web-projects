38aab6796b30749dbcd5620373b3e20ff0ab355c
rscoder999 <rscodes999@gmail.com>
2026-03-01 20:13:49 +0530

    Create script.js

---
+++ digital_clock/script.js
@@ -0,0 +1,24 @@
+function startTime(){
+    const today=new Date();
+
+    let h=today.getHours();
+    let m=today.getMinutes();
+    let s=today.getSeconds();
+
+
+    m=checkTime(m);
+    s=checkTime(s);
+
+    document.getElementById('clock').textContent=h+":"+m+":"+s;
+
+    setTimeout(startTime,1000);
+}
+
+function checkTime(i){
+    if(i<10){
+        i="0"+i;
+    }
+    return i;
+}
+
+//setInterval(startTime,1000)
